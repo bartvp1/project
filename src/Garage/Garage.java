e6d8c1515600ee7d98df2f87bdc8386327116d00
Albarod <r.vnw@hotmail.com>
2019-01-17 18:52:52 +0100

    Update Garage.java

--- src/Garage/Garage.java
+++ src/Garage/Garage.java
@@ -1,187 +1,402 @@
-package Control;
-
-import Garage.Garage;
-import Graph.GraphMainPanel;
-import Graph.GraphScrollContentPanel;
+package Garage;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
-/**
- * Control Panel beheert de controls
- * Bestaat uit twee delen:
- * 1. Een deel waar je de categorie kunt kiezen.
- * Categorieen: "Chart", "Simulator", "Control Panel", "About";
- * <p>
- * 2. Een deel wat bestaat uit de panel met de settings die bij de categorie hoort.
- * De card layout zorgt ervoor dat er wordt geswitched als een categorie wordt geklikt.
- * De verschillende settings (Panels) worden beheert in aparte classes onderaan de pagina.
- * <p>
- * Als het blijkt dat we weinig settings hebben kunnen we alles op 1 panel doen.
- */
-public class ControlPanel extends JPanel {
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    JLabel titleLabel = new JLabel("Control Panel", JLabel.CENTER);
-    String[] categories = {"Chart", "Simulator", "Control Panel", "About"};
-    Garage garage;
-    GraphMainPanel graph;
-    GraphScrollContentPanel graphContent;
-    JPanel settingsPanel;
+public class Garage extends JPanel implements Runnable {
+    Thread thread = new Thread(this);
+    private Dimension size;
+    private BufferedImage carParkImage;
+    private Car[][][] cars;
 
-    public ControlPanel(Garage garage, GraphMainPanel graph) {
-        super(null);
-        this.garage = garage;
-        this.graph = graph;
-        this.graphContent = graph.getContent();
+    private static final String AD_HOC = "1";
+    private static final String PASS = "2";
+
+    private CarQueue entranceCarQueue;
+    private CarQueue entrancePassQueue;
+    private CarQueue paymentCarQueue;
+    private CarQueue exitCarQueue;
+
+    private int numberOfFloors;
+    private int numberOfRows;
+    private int numberOfPlaces;
+    private int numberOfOpenSpots;
+    private int day = 0;
+    private int hour = 0;
+    private int minute = 0;
+
+
+    private int tickPause = 5;
+
+    int weekDayArrivals = 100; // average number of arriving cars per hour
+    int weekendArrivals = 200; // average number of arriving cars per hour
+    int weekDayPassArrivals = 50; // average number of arriving cars per hour
+    int weekendPassArrivals = 5; // average number of arriving cars per hour
+
+    int enterSpeed = 3; // number of cars that can enter per minute
+    int paymentSpeed = 7; // number of cars that can pay per minute
+    int exitSpeed = 5; // number of cars that can leave per minute
+
+
+    int numberOfCars = 0;
+    int numberOfPassCars = 0;
+    int numberOfNormalCars = 0;
+
+    JLabel countLabel = new JLabel();
+
+    public Garage(int numberOfFloors, int numberOfRows, int numberOfPlaces) {
+        this.numberOfFloors = numberOfFloors;
+        this.numberOfPlaces = numberOfPlaces;
+        this.numberOfRows = numberOfRows;
+        this.numberOfOpenSpots = numberOfFloors * numberOfRows * numberOfPlaces;
+        cars = new Car[numberOfFloors][numberOfRows][numberOfPlaces];
+
+        entranceCarQueue = new CarQueue();
+        entrancePassQueue = new CarQueue();
+        paymentCarQueue = new CarQueue();
+        exitCarQueue = new CarQueue();
+
+        countLabel.setBounds(10, 10, 200, 50);
+        add(countLabel);
     }
-
-    /**
-     * Grafiek Settings
-     * <p>
-     * Speed
-     * Show different Cars
-     * Change Grafiek style?
-     * Toggle scroll / repeat
-     * Start
-     * Stop
-     * Pause
-     * Reset
-     */
-
 
     public void init() {
-        //  grootte van dit scherm, breedte: 1/3 van  je beeldscherm & hoogte: 3/4 van je beeldscherm.
-        setSize((screenSize.width / 3), ((screenSize.height / 4) * 3));
-
-        // Wordt nog aangepast als we 't wat mooier maken
         setBackground(Color.DARK_GRAY);
-        setBorder(new LineBorder(Color.BLACK, 3, true));
 
-        // Panel waar je de category kan kiezen.
-        JPanel categoryPanel = new JPanel(new GridLayout(0, 1, 10, 50));
-        categoryPanel.setBounds(10, 10, 130, getHeight() - 25);
-        categoryPanel.setBackground(Color.DARK_GRAY);
-        add(categoryPanel);
+        size = new Dimension(1500, 750);
+        setPreferredSize(size);
 
-        //  Panel waar de settings staan
-        settingsPanel = new JPanel(new CardLayout());
-        settingsPanel.setBounds(150, 10, getWidth() - 160, getHeight() - 20);
-        settingsPanel.setBackground(Color.DARK_GRAY);
-        settingsPanel.setBorder(new LineBorder(Color.BLACK, 3, true));
-        add(settingsPanel);
-
-        createCategoryButtons(categoryPanel, settingsPanel);
-
-        // De verschillende menu's toevoegen aan de settingspanel
-        settingsPanel.add(new ChartSettings(), "Chart");
-        settingsPanel.add(new SimulatorSettings(), "Simulator");
-        settingsPanel.add(new ControlSettings(), "Control Panel");
-        settingsPanel.add(new AboutSettings(), "About");
-
-        // De eerste panel die je ziet is de chart settings
-        CardLayout cl = (CardLayout) settingsPanel.getLayout();
-        cl.show(settingsPanel, "Chart");
-
-        // leegje knopjes erin gooien zodat exitbutton onderaan kan staan
-        for (int i = 0; i < 3; i++) {
-            JPanel p = new JPanel();
-            p.setVisible(false);
-            categoryPanel.add(p);
-        }
-        // exit button sluit alles af
-        JButton exitButton = new JButton("Exit");
-        categoryPanel.add(exitButton);
-        exitButton.setBackground(new Color(225, 0, 0, 100));
-        exitButton.setFont(new Font("Arial", Font.PLAIN, 20));
-        exitButton.setForeground(new Color(0, 0, 0, 255));
-        exitButton.addActionListener(e -> System.exit(0));
+        carParkImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        thread.start();
+        repaint();
     }
 
-
-    /**
-     * @param categoryPanel de categorypanel waar de categories staan
-     * @param settingsPanel de settignspanel waar de settings staan.
-     *                      <p>
-     *                      Hij gaat alle categories bij langs en maakt voor elke een button, deze stopt ie in de categorypanel
-     *                      Als er op wordt geklikt, switched de panel naar de juist categorie
-     */
-    public void createCategoryButtons(JPanel categoryPanel, JPanel settingsPanel) {
-
-        for (String s : categories) {
-            JButton button = new JButton(s);
-            categoryPanel.add(button);
-            button.addActionListener(e -> {
-                CardLayout cl = (CardLayout) settingsPanel.getLayout();
-                cl.show(settingsPanel, s);
-            });
+    public void run() {
+        while (thread != null) {
+            tick();
+            try {
+                thread.sleep(tickPause);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    class ChartSettings extends JPanel {
-        //todo: snelheid van de grafiek, welke auto's je wilt zien en misschien een layout verandering?
-        JButton fillMode = new JButton("Fill");
-        JSlider speedSlider = new JSlider();
-        JPanel panel = new JPanel(new BorderLayout());
-        JPanel gridPanel = new JPanel(new GridLayout(0, 1));
+    public Car getCarAt(Location location) {
+        if (!locationIsValid(location)) {
+            return null;
+        }
+        return cars[location.getFloor()][location.getRow()][location.getPlace()];
+    }
 
-        public ChartSettings() {
-            setBackground(Color.DARK_GRAY);
-            add(gridPanel);
-            gridPanel.add(panel);
+    public int getTotalCars() {
+        return this.numberOfCars;
+    }
 
-            panel.setBorder(new LineBorder(Color.BLACK, 1, true));
-            add(panel);
-            panel.add(new JLabel("Snelheid", JLabel.CENTER), BorderLayout.NORTH);
-            panel.add(new JLabel("Langzaam"), BorderLayout.WEST);
-            panel.add(speedSlider, BorderLayout.CENTER);
-            panel.add(new JLabel("Snel"), BorderLayout.EAST);
+    private boolean locationIsValid(Location location) {
+        int floor = location.getFloor();
+        int row = location.getRow();
+        int place = location.getPlace();
+        if (floor < 0 || floor >= numberOfFloors || row < 0 || row > numberOfRows || place < 0 || place > numberOfPlaces) {
+            return false;
+        }
+        return true;
+    }
 
+    public void setTickPause(int value) {
+        this.tickPause = value;
+    }
 
-            gridPanel.add(fillMode);
-            fillMode.addActionListener(e -> graph.toggleFillMode());
+    public void updateView() {
+        // Create a new car park image if the size has changed.
 
-            speedSlider.addChangeListener(e -> {
-                int sliderValue = speedSlider.getValue();
-
-                sliderValue = 100 - sliderValue;
-                if (sliderValue <= 0) {
-                    sliderValue = 1;
+        if (carParkImage != null) {
+            Graphics graphics = carParkImage.getGraphics();
+            for (int floor = 0; floor < numberOfFloors; floor++) {
+                for (int row = 0; row < numberOfRows; row++) {
+                    for (int place = 0; place < numberOfPlaces; place++) {
+                        Location location = new Location(floor, row, place);
+                        Car car = getCarAt(location);
+                        Color color = car == null ? Color.white : car.getColor();
+                        drawPlace(graphics, location, color);
+                    }
                 }
-                graphContent.setTicks(sliderValue);
-            });
+            }
+            repaint();
+        }
 
-            JButton hideButton = new JButton("Hide/Show");
-            gridPanel.add(hideButton);
-            hideButton.addActionListener(e -> {
-                if (graph.isUp()) {
-                    graph.doHide();
-                } else if (graph.isDown()) {
-                    graph.doShow();
+    }
+
+    private void drawPlace(Graphics graphics, Location location, Color color) {
+        int lotWidth = 20;
+        int lotHeight = 10;
+
+        graphics.setColor(color);
+        graphics.fillRect(
+                location.getFloor() * 260 + (1 + (int) Math.floor(location.getRow() * 0.5)) * 75 + (location.getRow() % 2) * 20,
+                60 + location.getPlace() * lotHeight,
+                lotWidth - 1,
+                lotHeight - 1); // TODO use dynamic size or constants
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (carParkImage == null) {
+            return;
+        }
+//
+//        Dimension currentSize = getSize();
+//        if (size.equals(currentSize)) {
+//
+        g.drawImage(carParkImage, 0, 0, null);
+//        }
+//        \else {
+//            // Rescale the previous image.
+//            g.drawImage(carParkImage, 0, 0, currentSize.width, currentSize.height, null);
+//        }
+    }
+
+    private void tick() {
+        advanceTime();
+        handleExit();
+        updateViews();
+        handleEntrance();
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    private void advanceTime() {
+        // Advance the time by one minute.
+        minute++;
+        while (minute > 59) {
+            minute -= 60;
+            hour++;
+        }
+        while (hour > 23) {
+            hour -= 24;
+            day++;
+        }
+        while (day > 6) {
+            day -= 7;
+        }
+
+    }
+
+    private void handleEntrance() {
+        carsArriving();
+        carsEntering(entrancePassQueue, "Pass");
+        carsEntering(entranceCarQueue, "Normal");
+    }
+
+    private void handleExit() {
+        carsReadyToLeave();
+        carsPaying();
+        carsLeaving();
+    }
+
+    public void tick2() {
+        for (int floor = 0; floor < numberOfFloors; floor++) {
+            for (int row = 0; row < numberOfRows; row++) {
+                for (int place = 0; place < numberOfPlaces; place++) {
+                    Location location = new Location(floor, row, place);
+                    Car car = getCarAt(location);
+                    if (car != null) {
+                        car.tick();
+                    }
                 }
-
-            });
+            }
         }
     }
 
-    class SimulatorSettings extends JPanel {
-        //todo: snelheid van de de simulatie, aanpassingen aan het aantal ingangen, uitgangen etc.
-        public SimulatorSettings() {
-            setBackground(Color.RED);
+    private void updateViews() {
+        tick2();
+
+        updateView();
+    }
+
+    private void carsArriving() {
+        int numberOfCars = getNumberOfCars(weekDayArrivals, weekendArrivals);
+        addArrivingCars(numberOfCars, AD_HOC);
+        numberOfCars = getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
+        addArrivingCars(numberOfCars, PASS);
+    }
+
+    public Location getFirstFreeLocation() {
+        for (int floor = 0; floor < numberOfFloors; floor++) {
+            for (int row = 0; row < numberOfRows; row++) {
+                for (int place = 0; place < numberOfPlaces; place++) {
+                    Location location = new Location(floor, row, place);
+                    if (getCarAt(location) == null) {
+                        return location;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean setCarAt(Location location, Car car) {
+        if (!locationIsValid(location)) {
+            return false;
+        }
+        Car oldCar = getCarAt(location);
+        if (oldCar == null) {
+            cars[location.getFloor()][location.getRow()][location.getPlace()] = car;
+            car.setLocation(location);
+            numberOfOpenSpots--;
+            return true;
+        }
+        return false;
+    }
+
+    private void carsEntering(CarQueue queue, String type) {
+        int i = 0;
+        // Remove car from the front of the queue and assign to a parking space.
+
+        while (queue.carsInQueue() > 0 &&
+                numberOfOpenSpots > 0 &&
+                i < enterSpeed) {
+            Car car = queue.removeCar();
+            Location freeLocation = getFirstFreeLocation();
+//            System.out.println();
+            setCarAt(freeLocation, car);
+            i++;
+            numberOfCars++;
+            if (type.equals("Pass")) {
+
+                numberOfPassCars++;
+                countLabel.setText("Pass Cars: " + numberOfPassCars + ", Normal Cars: " + numberOfNormalCars + ", Total Cars: " + numberOfCars);
+            } else if (type.equals("Normal")) {
+                numberOfNormalCars++;
+                countLabel.setText("Pass Cars: " + numberOfPassCars + ", Normal Cars: " + numberOfNormalCars + ", Total Cars: " + numberOfCars);
+            }
         }
     }
 
-    class ControlSettings extends JPanel {
-        //todo: Controls van de control panel, als er niks in hoeft kan deze weg.
-        public ControlSettings() {
-            setBackground(Color.BLUE);
+    public int getNumberOfNormalCars() {
+        return numberOfNormalCars;
+    }
+
+    public int getNumberOfPassCars() {
+        return numberOfPassCars;
+    }
+
+    private void carsReadyToLeave() {
+        // Add leaving cars to the payment queue.
+        Car car = getFirstLeavingCar();
+        while (car != null) {
+            if (car.getHasToPay()) {
+                car.setIsPaying(true);
+                paymentCarQueue.addCar(car);
+
+            } else {
+                carLeavesSpot(car);
+                numberOfPassCars--;
+                countLabel.setText("Pass Cars: " + numberOfPassCars + ", Normal Cars: " + numberOfNormalCars + ", Total Cars: " + numberOfCars);
+            }
+            car = getFirstLeavingCar();
         }
     }
 
-    class AboutSettings extends JPanel {
-        //todo: Informatie over ons, dat we zo cool zijn enzo.
-        public AboutSettings() {
-            setBackground(Color.YELLOW);
+    public Car getFirstLeavingCar() {
+        for (int floor = 0; floor < numberOfFloors; floor++) {
+            for (int row = 0; row < numberOfRows; row++) {
+                for (int place = 0; place < numberOfPlaces; place++) {
+                    Location location = new Location(floor, row, place);
+                    Car car = getCarAt(location);
+                    if (car != null && car.getMinutesLeft() <= 0 && !car.getIsPaying()) {
+                        return car;
+                    }
+                }
+            }
         }
+        return null;
+    }
+
+    public Car removeCarAt(Location location) {
+        if (!locationIsValid(location)) {
+            return null;
+        }
+        Car car = getCarAt(location);
+        if (car == null) {
+            return null;
+        }
+        cars[location.getFloor()][location.getRow()][location.getPlace()] = null;
+        car.setLocation(null);
+        numberOfOpenSpots++;
+        return car;
+    }
+
+    private void carsPaying() {
+        // Let cars pay.
+        int i = 0;
+        while (paymentCarQueue.carsInQueue() > 0 && i < paymentSpeed) {
+            Car car = paymentCarQueue.removeCar();
+            // TODO Handle payment.
+            carLeavesSpot(car);
+            i++;
+            numberOfNormalCars--;
+            countLabel.setText("Pass Cars: " + numberOfPassCars + ", Normal Cars: " + numberOfNormalCars + ", Total Cars: " + numberOfCars);
+        }
+    }
+
+    private void carsLeaving() {
+        // Let cars leave.
+        int i = 0;
+        while (exitCarQueue.carsInQueue() > 0 && i < exitSpeed) {
+            exitCarQueue.removeCar();
+            i++;
+            numberOfCars--;
+        }
+    }
+
+    private int getNumberOfCars(int weekDay, int weekend) {
+        Random random = new Random();
+
+        // Get the average number of cars that arrive per hour.
+        int averageNumberOfCarsPerHour = day < 5
+                ? weekDay
+                : weekend;
+
+        // Calculate the number of cars that arrive this minute.
+        double standardDeviation = averageNumberOfCarsPerHour * 0.3;
+        double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
+        return (int) Math.round(numberOfCarsPerHour / 60);
+    }
+
+    private void addArrivingCars(int numberOfCars, String type) {
+        // Add the cars to the back of the queue.
+
+        switch (type) {
+            case AD_HOC:
+                for (int i = 0; i < numberOfCars; i++) {
+                    entranceCarQueue.addCar(new AdHocCar());
+                }
+                break;
+            case PASS:
+                for (int i = 0; i < numberOfCars; i++) {
+                    entrancePassQueue.addCar(new ParkingPassCar());
+                }
+                break;
+        }
+
+    }
+
+    private void carLeavesSpot(Car car) {
+        removeCarAt(car.getLocation());
+        exitCarQueue.addCar(car);
+//        System.out.println("Garage.Car left");
     }
 }
