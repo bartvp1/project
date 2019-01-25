1cef441b6cece1c92299b62871035b42ee0a54c2
bart <bartvp@live.nl>
2019-01-25 20:52:50 +0100

    Control Panel aangepast

--- src/Control/ControlPanel.java
+++ src/Control/ControlPanel.java
@@ -1,15 +1,12 @@
 package Control;
 
 import Garage.GarageModel;
-import Garage.Reservation;
 import Graph.GraphController;
-import MyComponents.CheckBox;
 import MyComponents.MyLabel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Control Panel beheert de controls
@@ -25,13 +22,11 @@ import java.awt.event.ActionListener;
  */
 public class ControlPanel extends JPanel {
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    private String[] categories = {"Simulator", "Reservations", "Chart","Control Panel", "About"};
+    private String[] categories = {"Control Panel","Reservations","About"};
 
     private GraphController graphController;
-    private JPanel settingsPanel;
-    JPanel categoryPanel = new JPanel(new GridLayout(0, 1, 10, 50));
     private GarageModel garage;
-    JLabel titleLabel = new JLabel("Configuratie", JLabel.CENTER);
+    private ArrayList<JButton> buttons = new ArrayList<>();
 
     public ControlPanel(GarageModel garage, GraphController graph) {
         super(null);
@@ -54,54 +49,56 @@ public class ControlPanel extends JPanel {
 
 
     public void init() {
-        setBackground(new Color(47, 49, 54));
+        super.setBackground(new Color(47, 49, 54));
 
+        JLabel titleLabel = new JLabel("Configuration", JLabel.CENTER);
         titleLabel.setBounds(0, 0, getWidth(), 50);
         titleLabel.setFont(new Font("Dubai Light", Font.PLAIN, 25));
         titleLabel.setForeground(Color.WHITE);
-        add(titleLabel);
+        super.add(titleLabel);
 
-        categoryPanel.setBounds(10, titleLabel.getHeight() + 10, 130, getHeight() - titleLabel.getHeight() - 30);
+        //Panel met de Configuratie Tabs
+        JPanel categoryPanel = new JPanel(new GridLayout(8, 1, 0, 20));
+        categoryPanel.setBounds(10, titleLabel.getHeight() + 10, 100, 620);
         categoryPanel.setBackground(Color.DARK_GRAY);
         categoryPanel.setOpaque(false);
-        add(categoryPanel);
+        super.add(categoryPanel);
 
-        //  Panel waar de settings staan
-        settingsPanel = new JPanel(new CardLayout());
+        //Panel met de settings
+        JPanel settingsPanel = new JPanel(new CardLayout());
         int settingsX = categoryPanel.getWidth() + categoryPanel.getX() + 10;
         settingsPanel.setBounds(settingsX, titleLabel.getHeight() + 10, getWidth() - settingsX - 10, getHeight() - titleLabel.getHeight() - 30);
         settingsPanel.setOpaque(false);
-        add(settingsPanel);
+        super.add(settingsPanel);
 
         createCategoryButtons(categoryPanel, settingsPanel);
 
         // De verschillende menu's toevoegen aan de settingspanel
-        settingsPanel.add(new ChartSettings(), "Chart");
-        SimulatorSettings simSet = new SimulatorSettings();
-        settingsPanel.add(simSet, "Simulator");
+        settingsPanel.add(new SimulatorSettings(), "Control Panel");
         settingsPanel.add(new ReservationPanel(), "Reservations");
-        settingsPanel.add(new ControlSettings(), "Control Panel");
         settingsPanel.add(new AboutSettings(), "About");
 
         // De eerste panel die je ziet is de chart settings
         CardLayout cl = (CardLayout) settingsPanel.getLayout();
-        cl.show(settingsPanel, "Simulator");
+        cl.show(settingsPanel, "Control Panel");
 
         // leegje knopjes erin gooien zodat exitbutton onderaan kan staan
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             JPanel p = new JPanel();
             p.setVisible(false);
             categoryPanel.add(p);
         }
         // exit button sluit alles af
         JButton exitButton = new JButton("Exit");
-        categoryPanel.add(exitButton);
         exitButton.setBackground(new Color(225, 0, 0));
         exitButton.setFont(new Font("Arial", Font.PLAIN, 20));
         exitButton.setForeground(new Color(0, 0, 0, 255));
-        exitButton.addActionListener(e -> System.exit(0));
+        exitButton.setBorder(null);
+        exitButton.setFocusPainted(false);
+        //exitButton.setAlignmentY(100);
+        categoryPanel.add(exitButton);
 
-        garage.addReservation(1, 2, 10);
+        exitButton.addActionListener(e -> System.exit(0));
     }
 
 
@@ -113,70 +110,33 @@ public class ControlPanel extends JPanel {
      *                      Als er op wordt geklikt, switched de panel naar de juist categorie
      */
     private void createCategoryButtons(JPanel categoryPanel, JPanel settingsPanel) {
-
         for (String s : categories) {
             JButton button = new JButton(s);
-            categoryPanel.add(button);
+            buttons.add(button);
+            button.setFocusPainted(false);
+            button.setBorder(null);
+            button.setForeground(Color.WHITE);
+            button.setContentAreaFilled(true);
+            if (s == "Control Panel") {
+                button.setBackground(new Color(0x454545));
+            } else {
+                button.setBackground(new Color(0x5D5D5D));
+            }
+
+            categoryPanel.add(button, BorderLayout.EAST);
+
             button.addActionListener(e -> {
                 CardLayout cl = (CardLayout) settingsPanel.getLayout();
                 cl.show(settingsPanel, s);
+                for (JButton clickedButton : buttons) {
+                    if (clickedButton.equals(button)) {
+                        clickedButton.setBackground(new Color(0x454545));
+                    } else {
+                        clickedButton.setBackground(new Color(0x5D5D5D));
+                    }
+                }
             });
         }
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(new Color(0, 0, 0, 150));
-        g2.setStroke(new BasicStroke(2));
-        g2.drawLine(getWidth() - 2, 0, getWidth() - 2, getHeight() - 4);
-        g2.drawLine(0, getHeight() - 2, getWidth(), getHeight() - 2);
-
-        g2.setStroke(new BasicStroke(.4f));
-        g.drawLine(0, 0, getWidth() - 1, 0);
-        g.drawLine(0, 0, 0, getHeight());
-    }
-
-    class ChartSettings extends JPanel {
-        //todo: snelheid van de grafiek, welke auto's je wilt zien en misschien een layout verandering?
-
-        JPanel panel = new JPanel(new GridLayout(1, 0));
-        JCheckBox fillMode = new JCheckBox("1", true);
-
-        ChartSettings() {
-            super(null);
-
-            setOpaque(false);
-            panel.setPreferredSize(new Dimension(50, 50));
-            panel.setSize(100, 10);
-            panel.setOpaque(false);
-
-
-            fillMode.setFont(new Font("Dubai Light", Font.PLAIN, 50));
-            fillMode.setForeground(Color.WHITE);
-            fillMode.setOpaque(false);
-//            fillMode.setBounds(100, 100, 100, 100);
-
-            panel.setOpaque(false);
-
-//            add(fillMode);
-
-            fillMode.addActionListener(e -> {
-                graphController.toggleFillMode();
-                fillMode.repaint();
-            });
-
-            CheckBox myCheckBox = new CheckBox(true);
-            myCheckBox.setSize(15.0);
-            myCheckBox.setBounds(100, 100, 100, 100);
-            add(myCheckBox);
-
-
-        }
-
     }
 
     class SimulatorSettings extends JPanel {
@@ -184,10 +144,10 @@ public class ControlPanel extends JPanel {
 
         SimulatorSettings() {
             super(new GridLayout(10, 1));
-            setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-            setBackground(new Color(43, 48, 52));
+            setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+            setBackground(new Color(0x454545));
 
-            JSlider speedSlider = new JSlider(0,3,0);
+            JSlider speedSlider = new JSlider(0, 3, 0);
 
             speedSlider.setMinorTickSpacing(1);
             speedSlider.setMajorTickSpacing(3);
@@ -198,9 +158,9 @@ public class ControlPanel extends JPanel {
 
             panel.setOpaque(false);
 
-            panel.add(new MyLabel("Tijd", JLabel.CENTER, "title_small"), BorderLayout.NORTH);
-            panel.add(new MyLabel("Langzaam", JLabel.CENTER, "description"), BorderLayout.WEST);
-            panel.add(new MyLabel("Snel", JLabel.CENTER, "description"), BorderLayout.EAST);
+            panel.add(new MyLabel("Simulation speed", JLabel.CENTER, "title_small"), BorderLayout.NORTH);
+            panel.add(new MyLabel("Slow", JLabel.CENTER, "description"), BorderLayout.WEST);
+            panel.add(new MyLabel("Fast", JLabel.CENTER, "description"), BorderLayout.EAST);
 
             panel.add(speedSlider, BorderLayout.CENTER);
             add(panel);
@@ -208,7 +168,7 @@ public class ControlPanel extends JPanel {
             speedSlider.addChangeListener(e -> {
                 int pause = speedSlider.getValue();
                 int newTickPause = 100;
-                switch(pause){
+                switch (pause) {
                     case 0:
                         newTickPause = 100;
                         break;
@@ -227,44 +187,67 @@ public class ControlPanel extends JPanel {
                 graphController.setTicks(newTickPause);
             });
 
-        }
+            //Panel
+            JPanel enterSpeed = new JPanel(new BorderLayout(10, 0));
+            enterSpeed.setOpaque(false);
+            JSlider enterSpeedSlider = new JSlider(0,20,garage.getEnterSpeed());
+            enterSpeedSlider.setOpaque(false);
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g;
-//            g.setColor(new Color(80, 85, 93));
-            g.setColor(new Color(0, 0, 0, 150));
+            enterSpeed.add(new MyLabel("Entering (cars/minute): " + enterSpeedSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
+            enterSpeed.add(new MyLabel("0", JLabel.CENTER, "description"), BorderLayout.WEST);
+            enterSpeed.add(new MyLabel("20", JLabel.CENTER, "description"), BorderLayout.EAST);
+            enterSpeed.add(enterSpeedSlider, BorderLayout.CENTER);
+            super.add(enterSpeed);
 
-            g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight());
-            g.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
+            enterSpeedSlider.addChangeListener(e -> {
+                int speed = enterSpeedSlider.getValue();
+                garage.setEnterSpeed(speed);
+                enterSpeed.remove(0);
+                enterSpeed.add(new MyLabel("Entering (cars/minute): " + enterSpeedSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
+            });
 
-            g2.setStroke(new BasicStroke(.1f));
-            g.drawLine(0, 0, getWidth() - 1, 0);
-            g.drawLine(0, 0, 0, getHeight());
 
+            //exits
+            JSlider totalExitsSlider = new JSlider(0,20,garage.getExitSpeed());
+            totalExitsSlider.setOpaque(false);
+
+            //Panel
+            JPanel exitSpeed = new JPanel(new BorderLayout(10, 0));
+            exitSpeed.setOpaque(false);
+
+            exitSpeed.add(new MyLabel("Leaving (cars/minute): " + totalExitsSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
+            exitSpeed.add(new MyLabel("0", JLabel.CENTER, "description"), BorderLayout.WEST);
+            exitSpeed.add(new MyLabel("20", JLabel.CENTER, "description"), BorderLayout.EAST);
+            exitSpeed.add(totalExitsSlider, BorderLayout.CENTER);
+            add(exitSpeed);
+            totalExitsSlider.addChangeListener(e -> {
+                int speed = totalExitsSlider.getValue();
+                garage.setExitSpeed(speed);
+                exitSpeed.remove(0);
+                exitSpeed.add(new MyLabel("Leaving (cars/minute): " + totalExitsSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
+            });
         }
     }
+
     class ReservationPanel extends JPanel {
         //todo: Reserveringen kunnen toegevoegd worden met dit menu
         ReservationPanel() {
-            super(new GridLayout(15, 5));
-            setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-            //setBackground(new Color(43, 48, 52));
+            super(new GridLayout(10, 1));
+            setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
             setBackground(new Color(0x454545));
 
-            String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+            String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
             Integer[] hours = new Integer[24];
             Integer[] minutes = new Integer[60];
-            for(int i=0;i<24;i++){hours[i]=i;}
-            for(int i=0;i<60;i++){minutes[i]=i;}
+            for (int i = 0; i < 24; i++) {
+                hours[i] = i;
+            }
+            for (int i = 0; i < 60; i++) {
+                minutes[i] = i;
+            }
 
-            JPanel title = new JPanel();
             JPanel panel = new JPanel();
-            title.setOpaque(false);
             panel.setOpaque(false);
-
-            title.add(new MyLabel("Reservering toevoegen", JLabel.CENTER, "title_small"), BorderLayout.NORTH);
 
             panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 
@@ -272,120 +255,38 @@ public class ControlPanel extends JPanel {
             JComboBox hourSelect = new JComboBox(hours);
             JComboBox minuteSelect = new JComboBox(minutes);
             JButton button = new JButton("add");
-            daySelect.setMaximumSize(new Dimension(80,20));
-            hourSelect.setMaximumSize(new Dimension(40,20));
-            minuteSelect.setMaximumSize(new Dimension(40,20));
-            button.setMaximumSize(new Dimension(40,20));
+            daySelect.setMaximumSize(new Dimension(80, 20));
+            hourSelect.setMaximumSize(new Dimension(40, 20));
+            minuteSelect.setMaximumSize(new Dimension(40, 20));
+            button.setMaximumSize(new Dimension(70, 20));
 
             panel.add(daySelect);
             panel.add(new MyLabel(" at ", JLabel.CENTER, "description"));
             panel.add(hourSelect);
             panel.add(new MyLabel(" : ", JLabel.CENTER, "description"));
             panel.add(minuteSelect);
-            panel.add(Box.createRigidArea(new Dimension(5,0)));
-            panel.add(button);
+            panel.add(Box.createRigidArea(new Dimension(45, 0)));
+            panel.add(button,BorderLayout.EAST);
+            super.add(panel);
 
-
-            add(title);
-            add(panel);
-
-            button.addActionListener(e ->  {
-                    int day = daySelect.getSelectedIndex();
-                    int hour = hourSelect.getSelectedIndex();
-                    int minute = minuteSelect.getSelectedIndex();
-                    garage.addReservation(day,hour,minute);
+            button.addActionListener(e -> {
+                int day = daySelect.getSelectedIndex();
+                int hour = hourSelect.getSelectedIndex();
+                int minute = minuteSelect.getSelectedIndex();
+                garage.addReservation(day, hour, minute);
             });
 
         }
 
-    }
-
-    class ControlSettings extends JPanel {
-        ControlSettings() {
-            super(new GridLayout(10, 1));
-            setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-            setBackground(new Color(43, 48, 52));
-
-
-            //Panel
-            JPanel panel = new JPanel(new BorderLayout(10, 0));
-            panel.setOpaque(false);
-
-            panel.add(new MyLabel("Waiting in Q: " + garage.getEntranceCarQueue().carsInQueue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-
-
-            add(panel);
-
-
-            JSlider enterSpeedSlider = new JSlider();
-            enterSpeedSlider.setMaximum(20);
-            enterSpeedSlider.setMinimum(0);
-            enterSpeedSlider.setValue(garage.getEnterSpeed());
-            enterSpeedSlider.setOpaque(false);
-
-            //Panel
-            JPanel panelEnterspeed = new JPanel(new BorderLayout(10, 0));
-            panelEnterspeed.setOpaque(false);
-
-            panelEnterspeed.add(new MyLabel("Entrance: " + enterSpeedSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-            panelEnterspeed.add(new MyLabel("0", JLabel.CENTER, "description"), BorderLayout.WEST);
-            panelEnterspeed.add(new MyLabel("20", JLabel.CENTER, "description"), BorderLayout.EAST);
-            panelEnterspeed.add(enterSpeedSlider, BorderLayout.CENTER);
-
-            add(panelEnterspeed);
-            enterSpeedSlider.addChangeListener(e -> {
-                int speed = enterSpeedSlider.getValue();
-                garage.setEnterSpeed(speed);
-                panel.updateUI();
-                panelEnterspeed.remove(0);
-                panelEnterspeed.add(new MyLabel("Entrance: " + enterSpeedSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-
-
-                panel.removeAll();
-                panel.add(new MyLabel("Waiting in Q: " + garage.getEntranceCarQueue().carsInQueue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-
-            });
-
-
-            //exits
-            JSlider totalExitsSlider = new JSlider();
-            totalExitsSlider.setMaximum(20);
-            totalExitsSlider.setMinimum(0);
-            totalExitsSlider.setValue(garage.getExitSpeed());
-            totalExitsSlider.setOpaque(false);
-
-            //Panel
-            JPanel panelTotalExits = new JPanel(new BorderLayout(10, 0));
-            panelTotalExits.setOpaque(false);
-
-            panelTotalExits.add(new MyLabel("Exits: " + totalExitsSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-            panelTotalExits.add(new MyLabel("0", JLabel.CENTER, "description"), BorderLayout.WEST);
-            panelTotalExits.add(new MyLabel("20", JLabel.CENTER, "description"), BorderLayout.EAST);
-            panelTotalExits.add(totalExitsSlider, BorderLayout.CENTER);
-
-            add(panelTotalExits);
-            totalExitsSlider.addChangeListener(e -> {
-                int speed = totalExitsSlider.getValue();
-                garage.setExitSpeed(speed);
-
-                panelTotalExits.remove(0);
-                panelTotalExits.add(new MyLabel("Exits: " + totalExitsSlider.getValue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-
-
-                panel.removeAll();
-                panel.add(new MyLabel("Waiting in Q: " + garage.getEntranceCarQueue().carsInQueue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-
-            });
-
-            panel.removeAll();
-            panel.add(new MyLabel("Waiting in Q: " + garage.getEntranceCarQueue().carsInQueue(), JLabel.CENTER, "title_small"), BorderLayout.NORTH, 0);
-        }
     }
 
     class AboutSettings extends JPanel {
         //todo: Informatie over ons, dat we zo cool zijn enzo.
         AboutSettings() {
-            setBackground(Color.YELLOW);
+            setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+            setBackground(new Color(0x454545));
+            add(new MyLabel("About", JLabel.CENTER, "title_small"), BorderLayout.NORTH);
+            add(new MyLabel("<html>Deze parkeersimulator is gemaakt door: <br><ul><li>Vincent Huisman</li><li>Riet van Noordt Wieringa</li><li>Jarno Steursma</li><li>Bart van Poele</li></ul> </html>", JLabel.CENTER, "description"));
         }
     }
 }
