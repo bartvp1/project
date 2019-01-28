2fe59f505d39344ef218951d41dee13d6ea745ca
Riet <r237550xe5@gmail.com>
2019-01-28 18:19:59 +0100

    icons enzo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Main
+ * Entry Point
+ * Frame wordt gemaakt, MainPanel als contentpane,
+ * Frame is fullscreen
+ */
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        MainPanel panel = new MainPanel();
+        // grootte van het beeldscherm
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        // het frame
+        JFrame frame = new JFrame();
         frame.setSize(screenSize.width, screenSize.height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(panel);
-        panel.init();
         frame.setUndecorated(true);
         frame.setResizable(false);
+
+        // main panel
+        MainPanel panel = new MainPanel();
+        panel.init();
+
+        // set main panel & visible
+        frame.setContentPane(panel);
         frame.setVisible(true);
     }
 }
